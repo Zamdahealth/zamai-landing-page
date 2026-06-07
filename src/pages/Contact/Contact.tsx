1aca8fed90045412209877a48471a12d4a2082b8
akachukwu-web <akachukwumba04@gmail.com>
2026-06-06 20:29:57 -0700

    Add new assets for dropdown, general inquiry, partnership, and technical support; update Contact page to include HearFromYou component

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -1,11 +1,13 @@
 import Faqs from "../../components/contact/Faqs/Faqs";
 import HearFromYou from "../../components/contact/HearFromYou/HearFromYou";
+// import MessageUs from "../../components/contact/MessageUs/MessageUs";
 import ReachUs from "../../components/contact/ReachUs/ReachUs";
 
 function Contact() {
   return (
     <>
      <HearFromYou />
+     {/* <MessageUs /> */}
      <ReachUs />
      <Faqs />
     </>
